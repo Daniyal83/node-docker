2460187ad686c2174216e2b4863f32ff81bd2452
Daniyal83 <da-lu@lenta.ru>
2022-05-08 14:11:30 +0300

    added image to node-app

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
     console.log("Yeah it ran");
-    res.send("<h2>Hi there!!!</h2>");
+    res.send("<h2>Hi there</h2>");
 })
 
 app.use("/api/v1/posts", postRouter);
